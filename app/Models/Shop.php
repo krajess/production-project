d3cc42c99453d8cdcd59cad89c19388baad65a64
krajess <krajes022@gmail.com>
2025-01-28 19:08:00 +0000

    create Shop model with fillable attributes and products relationship

---
+++ app/Models/Shop.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}

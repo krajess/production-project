6ce967b1709ac3c43239f32a8a9f9e4e133672db
krajess <krajes022@gmail.com>
2025-01-30 22:26:15 +0000

    add roles migration to manage user permissions

---
+++ database/migrations/2025_01_30_213220_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_business_owner')->default(false);
+            $table->boolean('is_business_manager')->default(false);
+            $table->boolean('is_customer')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
+            $table->dropColumn('is_business_owner');
+            $table->dropColumn('is_business_manager');
+            $table->dropColumn('is_customer');
+        });
+    }
+};

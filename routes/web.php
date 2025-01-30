c0e81ce433993bee3609e80cdd35c341d8add570
krajess <krajes022@gmail.com>
2025-01-30 22:26:36 +0000

    add shops route and admin access control

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/shops', function () {
+    return view('shops.index');
+})->name('shops.index');
+
+Route::middleware(['auth', 'can:is-admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

88a0b5df6e80313c42a5d927097a2a4b81bd6138
krajess <krajes022@gmail.com>
2025-01-31 19:50:14 +0000

    add shop controller methods for index, show, create, update, and destroy actions

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,10 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
+    public function index()
+    {
+        $shops = Shop::all();
+        return view('shops.index', compact('shops'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Shop $shop)
+    {
+        return view('shops.show', compact('shop'));
+    }
+
+    public function edit(Shop $shop)
+    {
+        //
+    }
+
+    public function update(Request $request, Shop $shop)
+    {
+       //
+    }
+
+    public function destroy(Shop $shop)
+    {
+        //
+    }
 }

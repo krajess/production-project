dea064680a50171a59fc28ac0ce2bfd3e0b57dea
krajess <krajes022@gmail.com>
2025-01-31 19:50:19 +0000

    add ShopOwnerController with index method and view

---
+++ app/Http/Controllers/ShopOwnerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ShopOwnerController extends Controller
+{
+    public function index()
+    {
+        return view('shop_owner.index');
+    }
+
+}

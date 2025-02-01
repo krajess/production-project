1fe52c634a3dd9c3a30d0ca14413faa23242281e
krajess <krajes022@gmail.com>
2025-02-01 20:09:45 +0000

    add shops management view for admin panel

---
+++ resources/views/admin/shops.blade.php
@@ -0,0 +1,71 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Businesses') }}
+        </h2>
+    </x-slot>
+
+    <div class="flex">
+        <div class="w-1/6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 m-4 flex flex-col items-center">
+            <div class="text-gray-900 dark:text-gray-100 w-full">
+                <div class="text-center font-bold mb-4">
+                    <h2> {{ __("MANAGEMENT MENU") }} </h2>
+                </div>
+                <a href="{{ route('admin.index') }}" class="btn-dark mb-2 block text-center">Dashboard</a>
+                <a href="{{ route('admin.shops') }}" class="btn-dark mb-2 block text-center">Businesses</a>
+                <a href="{{ route('admin.index') }}" class="btn-dark mb-2 block text-center">Applications</a>
+                <a href="{{ route('admin.users') }}" class="btn-dark mb-2 block text-center">Users</a>
+            </div>
+        </div>
+
+    <div class="w-4/5 py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 text-center">
+                    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 text-center dark:text-gray-400 uppercase tracking-wider">
+                                    Shop
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 text-center dark:text-gray-400 uppercase tracking-wider">
+                                    Description
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 text-center dark:text-gray-400 uppercase tracking-wider">
+                                    Owner ID
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 text-center dark:text-gray-400 uppercase tracking-wider">
+                                    Owner Email
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 text-center dark:text-gray-400 uppercase tracking-wider">
+                                    View
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white dark:bg-gray-800 divide-y divide-gray-200 dark:divide-gray-700">
+                            @foreach ($shops as $shop)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ $shop->name }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ $shop->description }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ $shop->owner_id }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        {{ $shop->owner->email }}
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <a href="{{ route('shops.show', $shop->id) }}" class="btn-dark">View</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

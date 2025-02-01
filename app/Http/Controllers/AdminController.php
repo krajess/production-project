2cb86fbf130e45967e32326fb1209a4cdfdb794d
krajess <krajes022@gmail.com>
2025-02-01 20:09:23 +0000

    add methods to AdminController for managing users and shops

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Shop;
 
 class AdminController extends Controller
 {
     public function index()
     {
         return view('admin.index');
+    }
+
+    public function users()
+    {
+        $users = User::where('is_admin', '!=', 1)->get();
+        return view('admin.users', compact('users'));
+    }
+
+    public function shops()
+    {
+        $shops = Shop::with('owner')->get();
+        return view('admin.shops', compact('shops'));
     }
 }

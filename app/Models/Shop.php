84f29a53f0dfe2e0957e80185b631b2804714203
krajess <krajes022@gmail.com>
2025-02-01 20:09:34 +0000

    add owner relationship to Shop model

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -2,15 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'description',
         'owner_id',
     ];
+
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 
     public function products()
     {

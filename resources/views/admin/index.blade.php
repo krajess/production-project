15071b15d2174e6e43e014a7ca9248c56a9ee87e
krajess <krajes022@gmail.com>
2025-02-11 00:21:02 +0000

    refactor admin dashboard layout for improved structure and styling

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,8 +5,20 @@
         </h2>
     </x-slot>
     
-    <x-nav-menu-admin>
+    <div class="flex">
+        <x-nav-menu-admin>
+            
+        </x-nav-menu-admin>
 
-    </x-nav-menu-admin>
+
+    <div class="w-4/5 py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <p> Welcome to the admin dashboard! </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </x-app-layout>

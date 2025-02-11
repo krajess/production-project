f1290b88581bb169d481f3e6ce4aaadda8fc184b
krajess <krajes022@gmail.com>
2025-02-11 00:20:50 +0000

    create applications table for user application management

---
+++ database/migrations/2025_02_10_235525_create_applications_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('status')->default('pending');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('postcode');
+            $table->string('country');
+            $table->string('business_name');
+            $table->string('business_type');
+            $table->string('business_description');
+            $table->string('business_experience');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};

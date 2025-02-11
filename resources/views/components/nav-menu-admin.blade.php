65a8dbbea90015cec88264f12c0d6e074bf20b94
krajess <krajes022@gmail.com>
2025-02-11 00:21:25 +0000

    update admin navigation menu styles for improved visibility

--- resources/views/components/nav-menu-admin.blade.php
+++ resources/views/components/nav-menu-admin.blade.php
@@ -1,7 +1,5 @@
 @if (auth()->user()->is_admin)
-
-<div class="flex">
-    <div class="w-2/6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 m-4 flex flex-col items-center">
+    <div class="w-1/7 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 m-4 flex flex-col items-center">
         <div class="text-gray-900 dark:text-gray-100 w-full">
             <div class="text-center font-bold mb-4">
                 <h2> {{ __("MANAGEMENT MENU") }} </h2>
@@ -11,24 +9,23 @@
             <div x-data="{ open: false }" class="w-full">
                 <button @click="open = !open" class="btn-dark mb-2 block text-center w-full">Businesses</button>
                 <div x-show="open" class="mt-2 space-y-2">
-                    <a href="{{ route('admin.shops') }}" class="btn-dark block text-center">View Businesses</a>
+                    <a href="{{ route('admin.shops') }}" class="btn-bright block text-center">View Businesses</a>
                 </div>
             </div>
 
             <div x-data="{ open: false }" class="w-full">
                 <button @click="open = !open" class="btn-dark mb-2 block text-center w-full">Applications</button>
                 <div x-show="open" class="mt-2 space-y-2">
-                    <a href="{{ route('admin.index') }}" class="btn-dark mb-2 block text-center">View Applications</a>
+                    <a href="{{ route('admin.index') }}" class="btn-bright mb-2 block text-center">View Applications</a>
                 </div>
             </div>
 
             <div x-data="{ open: false }" class="w-full">
                 <button @click="open = !open" class="btn-dark mb-2 block text-center w-full">Users</button>
                 <div x-show="open" class="mt-2 space-y-2">
-                    <a href="{{ route('admin.users') }}" class="btn-dark mb-2 block text-center">View Users</a>
+                    <a href="{{ route('admin.users') }}" class="btn-bright mb-2 block text-center">View Users</a>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endif

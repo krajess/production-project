46d3ddb33ec3b7352db312ef61db078172cdce72
krajess <krajes022@gmail.com>
2025-02-11 20:54:10 +0000

    add show application view for admin dashboard

---
+++ resources/views/admin/show-application.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('ADMIN DASHBOARD - Applications') }}
+        </h2>
+    </x-slot>
+
+    <div class="flex">
+        <x-nav-menu-admin>
+
+        </x-nav-menu-admin>
+
+    <div class="w-4/5 py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 text-center">
+                    <h1>{{ $application->first_name }} {{ $application->last_name }}</h1>
+                    <p>Email: {{ $application->email }}</p>
+                    <p>Phone: {{ $application->phone }}</p>
+                    <p>Address: {{ $application->address }}</p>
+                    <p>City: {{ $application->city }}</p>
+                    <p>Postcode: {{ $application->postcode }}</p>
+                    <p>Country: {{ $application->country }}</p>
+                    <p>Business Name: {{ $application->business_name }}</p>
+                    <p>Business Type: {{ $application->business_type }}</p>
+                    <p>Business Description: {{ $application->business_description }}</p>
+                    <p>Business Experience: {{ $application->business_experience }}</p>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

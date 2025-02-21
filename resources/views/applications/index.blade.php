1b2004d0bae0c67997622cbbfa9f5818737a1178
krajess <krajes022@gmail.com>
2025-02-21 17:29:09 +0000

    add conditional display for business owner status in applications view

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -5,6 +5,7 @@
         </h2>
     </x-slot>
 
+    @if(Auth::user()->is_business_owner != 1)
     <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -15,4 +16,15 @@
             </div>
         </div>
     </div>
+    @else
+    <div class="py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <p> You are already a business owner. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 </x-app-layout>

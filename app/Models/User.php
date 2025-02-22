9aea3730f312efda6eac081b3b3eac5c8123552d
krajess <krajes022@gmail.com>
2025-02-22 23:53:50 +0000

    add shops relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function shops()
+    {
+        return $this->hasMany(Shop::class, 'owner_id');
+    }
 }

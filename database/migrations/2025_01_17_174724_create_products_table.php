c5e7fc649f9e17c9ff7bc9488e5036852d87c40c
krajess <krajes022@gmail.com>
2025-02-25 20:29:09 +0000

    add product fields and shop relationship to products table

--- database/migrations/2025_01_17_174724_create_products_table.php
+++ database/migrations/2025_01_17_174724_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->foreignId('shop_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }

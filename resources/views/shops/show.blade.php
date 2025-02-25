25d0e794ad2c1457052146fb1e9183665f914905
krajess <krajes022@gmail.com>
2025-02-25 20:29:51 +0000

    add visibility toggle for shops in user view

--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -7,6 +7,22 @@
     <h1>{{ $shop->name }}</h1>
     <p>{{ $shop->description }}</p>
     <p>Owner ID: {{ $shop->owner_id }}</p>
+
+    <h2>Products</h2>
+    @if($shop->products->isEmpty())
+        <p>No products available for this shop.</p>
+    @else
+        <ul>
+            @foreach($shop->products as $product)
+                <li>
+                    <h3>{{ $product->name }}</h3>
+                    <p>{{ $product->description }}</p>
+                    <p>Price: £{{ $product->price }}</p>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+
     <a href="{{ route('shops.index') }}">Back to Shops</a>
 </body>
 </html>

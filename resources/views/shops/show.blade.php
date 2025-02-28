4bcc7d4702fd244f7fa64709f64b7f0c12bc46f0
krajess <krajes022@gmail.com>
2025-02-28 21:12:04 +0000

    refactor shop details view to use Blade components for improved layout

--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -1,10 +1,21 @@
-<!DOCTYPE html>
-<html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ $shop->name }}
+        </h2>
+    </x-slot>
+
+    <div class="w-1/6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 m-4 flex flex-col items-center">
+        <div class="text-gray-900 dark:text-gray-100 w-full">
+            <h2> Filters </h2>
+            
+        </div>
+    </div>
+
 <head>
     <title>Shop Details</title>
 </head>
 <body>
-    <h1>{{ $shop->name }}</h1>
     <p>{{ $shop->description }}</p>
     <p>Owner ID: {{ $shop->owner_id }}</p>
 
@@ -22,7 +33,5 @@
             @endforeach
         </ul>
     @endif
-
-    <a href="{{ route('shops.index') }}">Back to Shops</a>
 </body>
-</html>
+</x-app-layout>

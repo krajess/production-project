e9a552d085cab28ea88cbd140632649a6646afd7
krajess <krajes022@gmail.com>
2025-03-08 20:55:28 +0000

    add cart routes for viewing and managing cart items

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ShopController;
 use App\Http\Controllers\ShopOwnerController;
 use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -38,6 +39,13 @@ Route::middleware(['auth', 'can:is-business-owner'])->group(function () {
     Route::get('/shops/{shop}/products/create', [ProductController::class, 'create'])->name('products.create');
     Route::post('/shops/{shop}/products', [ProductController::class, 'store'])->name('products.store');
     Route::get('/shops/{shop}/products/{product}', [ProductController::class, 'show'])->name('shops.products.show');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
+    Route::patch('/cart/update/{product}', [CartController::class, 'update'])->name('cart.update');
+    Route::delete('/cart/remove/{product}', [CartController::class, 'remove'])->name('cart.remove');
 });
 
 Route::middleware('auth')->group(function () {

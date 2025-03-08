97c31ada9327581fda3b09d030113c21b7293a67
krajess <krajes022@gmail.com>
2025-03-08 20:24:09 +0000

    update product show method to include shop context

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -50,9 +50,9 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show(Shop $shop, Product $product)
     {
-        //
+        return view('products.show', compact('shop', 'product'));
     }
 
     /**

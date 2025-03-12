845bb6ff2d22f34bb388e5da64ef7fef775e5327
krajess <krajes022@gmail.com>
2025-03-12 22:59:50 +0000

    add stock column to products table for inventory management

--- database/migrations/2025_01_17_174724_create_products_table.php
+++ database/migrations/2025_01_17_174724_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
+            $table->integer('stock');
             $table->foreignId('shop_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });

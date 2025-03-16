0c2bcbdabe42bb8f6fb8bf9e987199a7d14f4e5c
krajess <krajes022@gmail.com>
2025-03-16 19:51:13 +0000

    remove shop_name column from products table to streamline database structure

--- database/migrations/2025_01_17_174724_create_products_table.php
+++ database/migrations/2025_01_17_174724_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->decimal('price', 8, 2);
             $table->integer('stock');
             $table->foreignId('shop_id')->constrained()->onDelete('cascade');
-            $table->string('shop_name');
             $table->timestamps();
         });
     }

a6d463014dd12b7e8a418127339c0ed10dfb3700
krajess <krajes022@gmail.com>
2025-03-17 19:09:12 +0000

    add order summary component to nav cart menu

---
+++ resources/views/components/nav-cart-menu.blade.php
@@ -0,0 +1,5 @@
+<div class="w-1/6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 m-4 flex flex-col items-center">
+    <div class="text-gray-900 dark:text-gray-100 w-full">
+        <h2> Order Summary </h2>
+    </div>
+</div>

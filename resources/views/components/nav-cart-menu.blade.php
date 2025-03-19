40fb9c4120d74b76635d9eab0418171b2196afb8
krajess <krajes022@gmail.com>
2025-03-19 22:12:23 +0000

    enhance order summary component in nav cart menu with improved layout and styling

--- resources/views/components/nav-cart-menu.blade.php
+++ resources/views/components/nav-cart-menu.blade.php
@@ -1,5 +1,13 @@
-<div class="w-1/6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 m-4 flex flex-col items-center">
-    <div class="text-gray-900 dark:text-gray-100 w-full">
-        <h2> Order Summary </h2>
+<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 m-4 flex flex-col items-center" style="width: 250px;">
+    <div class="text-gray-900 dark:text-gray-100 w-full text-center">
+        <h1 style="font-size: 20px; font-weight: 900;">ORDER SUMMARY</h1>
+        <hr class="my-2 border-gray-300 dark:border-gray-600 w-full">
+        <p class="text-gray-600 dark:text-gray-400">SHOP NAME</p>
+        <hr class="my-2 border-gray-300 dark:border-gray-600 w-full">
+        <p class="text-gray-600 dark:text-gray-400">TOTAL AMOUNT OF PRODUCTS</p>
+        <hr class="my-2 border-gray-300 dark:border-gray-600 w-full">
+        <p class="text-gray-600 dark:text-gray-400">TOTAL PRICE FOR PRODUCTS</p>
+        <hr class="my-2 border-gray-300 dark:border-gray-600 w-full">
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-20 w-full">Checkout</button>
     </div>
 </div>

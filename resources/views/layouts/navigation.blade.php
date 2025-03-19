081322d6ea06dfd3c1126f337de1c01ca2452b44
krajess <krajes022@gmail.com>
2025-03-19 22:12:34 +0000

    add link to cart in user dropdown menu for easier access

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -30,7 +30,7 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 @if (auth()->check())
                     <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
+                        <x-slot name="trigger"> 
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                                 <div>{{ Auth::user()->email }}</div>
 
@@ -47,12 +47,15 @@
                                     {{ __('Admin Dashboard') }}
                                 </x-dropdown-link>
                             @endif
-
                             @if(Auth::user()->is_business_owner==1)
                                 <x-dropdown-link :href="route('shop_owner.index')">
                                     {{ __('Shop Dashboard') }}
                                 </x-dropdown-link>
                             @endif
+
+                            <x-dropdown-link :href="route('cart.index')">
+                                {{ __('Your Cart') }}
+                            </x-dropdown-link>
 
                             <x-dropdown-link :href="route('profile.edit')">
                                 {{ __('Profile') }}

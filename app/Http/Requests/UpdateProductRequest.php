7c8295b2ba6e78f14aef010600c3915aa58c6dd3
krajess <krajes022@gmail.com>
2025-04-02 19:12:07 +0100

    add validation rules for product update request

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -26,7 +26,11 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+            'product_type_id' => 'required|exists:product_types,id',
         ];
     }
 }

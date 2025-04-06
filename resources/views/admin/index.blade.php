96635bd68cad3089b4aedc231e2e9950ff88b009
krajess <krajes022@gmail.com>
2025-04-06 16:26:00 +0100

    enhance admin dashboard with detailed application statistics and improved layout

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,24 +1,69 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('ADMIN DASHBOARD') }}
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
+            {{ __('ADMIN MANAGEMENT DASHBOARD') }}
         </h2>
     </x-slot>
     
     <div class="flex">
-        <x-nav-menu-admin>
+        <div class="w-1/6">
+            <x-nav-menu-admin>
+            </x-nav-menu-admin>
+        </div>
+
+        <div class="w-3/6 flex flex-col space-y-4 center">
+            <div class="w-full py-4">
+                <div class="max-w mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <h1 class="text-center"> USER STATISTICS</h1>
+                            <p>Total Registered Users: {{ $total_users }}</p>
+                            <p>Last Registered User: {{ $last_user->first_name }} {{ $last_user->last_name }}</p>
+                            <p>Total Vendor Owners: {{$total_vendor_owners}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full py-4">
+                <div class="max-w mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <h1> PRODUCTS STATISTICS</h1>
+                            <p>Total Products: {{ $total_products }}</p>
+                            <p>Last Added Product: {{ $last_product }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full py-4">
+                <div class="max-w mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <h1> VENDOR STATISTICS</h1>
+                            <p>Total Vendors: {{ $total_vendors }}</p>
+                            <p>Last Added Vendor: {{ $last_vendor }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             
-        </x-nav-menu-admin>
-
-
-    <div class="w-4/5 py-4">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <p> Welcome to the admin dashboard! </p>
+            <div class="w-full py-4">
+                <div class="max-w mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <h1> APPLICATION STATISTICS</h1>
+                            <p>Total Applications: {{ $total_applications }}</p>
+                            <p>Last Application: {{ $last_application->email }}</p>
+                            <p>Applications Accepted: {{ $accepted_applications }}</p>
+                            <p>Applications Rejected: {{ $rejected_applications }}</p>
+                            <p>Applications Pending: {{ $pending_applications }}</p>
+                            <p>Applications New: {{ $new_applications }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
 </x-app-layout>

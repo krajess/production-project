e5fb51906acdf6eefa0599e840b6c51cdbc70f70
krajess <krajes022@gmail.com>
2025-04-06 16:26:16 +0100

    update users table header to clarify name column as 'First & Last Name'

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -21,7 +21,7 @@
                                     ID
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
-                                    Name
+                                    First & Last Name
                                 </th>
                                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
                                     Email

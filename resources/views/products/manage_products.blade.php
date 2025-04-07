703bbe8b9317e3ca8f74a40fd9e1be7ffc000043
krajess <krajes022@gmail.com>
2025-04-07 22:34:01 +0100

    add manage products view with image display and delete functionality

--- resources/views/products/manage_products.blade.php
+++ resources/views/products/manage_products.blade.php
@@ -1,13 +1,17 @@
-<x-app-layout>
+   <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
             {{ $vendor->name }}
         </h2>
     </x-slot>
-        
-        <div class="flex flex-wrap justify-start w-full">
-            @if($vendor->products->isEmpty())
-                <div class="py-12">
+
+    <div class="flex">
+            <x-nav-filter-menu :vendor="$vendor">
+            </x-nav-filter-menu>
+
+        <div class="flex flex-wrap justify-start w-3/4">
+            @if($products->isEmpty())
+                <div class="py-4 w-full text-center">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                             <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -17,8 +21,9 @@
                     </div>
                 </div>
             @else
-                @foreach($vendor->products as $product)
-                    <div class="border rounded-lg p-4 flex flex-col bg-white shadow-md m-2" style="flex: 1 1 calc(33.33% - 2rem); max-width: calc(33.33% - 2rem); box-sizing: border-box; margin: 1rem;">
+                @foreach($products as $product)
+                    <div class="border rounded-lg p-2 flex flex-col bg-white shadow-m m-1" 
+                        style="flex: 1 1 calc(25% - 0.5rem); max-width: calc(25% - 0.5rem); box-sizing: border-box; margin-top: 15px;">
                         <div class="relative" style="height: 200px; overflow: hidden;">
                             @if (!empty($product->images) && is_array($product->images))
                                 <img id="mainImage-{{ $product->id }}" 
@@ -45,12 +50,12 @@
                         @endif
                         <div class="flex flex-col w-full">
                             <div class="flex justify-between items-center mb-1">
-                                <p class="font-bold text-md">{{ $product->name }}</p>
-                                <p class="bg-blue-200 text-black px-2 mr-2 rounded border border-black">{{ $product->product_types_name }}</p>
+                                <p class="font-bold" style="font-size: 20px;">{{ $product->name }}</p>
+                                <p class="bg-blue-200 text-black px-2 mr-2 rounded border border-black" style="font-size: 11px;">{{ $product->product_types_name }}</p>
                             </div>
-                            <p class="text-gray-700 mb-1">{{ $product->description }}</p>
+                            <p class="text-gray-700 mb-1 product-description">{{ $product->description }}</p>
                             <p class="text-green-500 font-semibold mb-3">&pound {{ number_format($product->price, 2) }}</p>
-                            <p class="text-gray-700 mb-3">Stock: {{ $product->stock }}</p>
+                            <p class="text-black-300 font-semibold mb-3" style="font-size: 13px;">Stock - {{ $product->stock }}</p>
                             <div class="flex justify-between items-center mt-auto">
                                 <a href="{{ route('products.edit', ['vendor' => $vendor->id, 'product' => $product->id]) }}" class="bg-blue-500 text-white px-4 py-2 rounded w-full text-center" style="margin-bottom: 10px;">EDIT PRODUCT</a>
                             </div>
@@ -69,3 +74,29 @@
         </div>
     </div>
 </x-app-layout>
+
+<style>
+    input[type=number]::-webkit-outer-spin-button,
+    input[type=number]::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+
+    input[type=number]:focus {
+    outline: none;
+    box-shadow: none;
+    }
+
+    .product-description {
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: normal;
+    }
+</style>

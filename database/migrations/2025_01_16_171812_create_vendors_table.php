e2aaac2fb30609246c4c223a7acd5f4afa075000
krajess <krajes022@gmail.com>
2025-04-08 20:52:11 +0100

    add email and color customization fields to vendors table

--- database/migrations/2025_01_16_171812_create_vendors_table.php
+++ database/migrations/2025_01_16_171812_create_vendors_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->string('email')->unique();
             $table->boolean('visible')->default(false);
+            $table->string('background_color')->nullable()->default('#ffffff');
+            $table->string('text_color')->nullable()->default('#000000');
+            $table->string('description_text_color')->nullable()->default('#000000');
+            $table->string('button_text_color')->nullable()->default('#ffffff');
+            $table->string('button_background_color')->nullable()->default('#000000');
             $table->timestamps();
         });
     }

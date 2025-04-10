47d412f40025ad4732c944af80317a001d1e424e
krajess <krajes022@gmail.com>
2025-04-10 22:43:45 +0100

    refactor Order model to define fillable attributes and establish product relationships

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+
+    protected $fillable = [
+        'user_id',
+        'total_price',
+        'status',
+    ];
+
+    public function products()
+{
+    return $this->belongsToMany(Product::class)->withPivot('quantity')->withTimestamps();
+}
 }

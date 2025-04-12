67d861b9b44be7b38dc93c6638b921265f94ef27
krajess <krajes022@gmail.com>
2025-04-12 01:22:18 +0100

    reduce product name line clamp to improve display consistency

--- resources/views/products/show_products.blade.php
+++ resources/views/products/show_products.blade.php
@@ -108,7 +108,7 @@
 
     .product-name {
         display: -webkit-box;
-        -webkit-line-clamp: 2;
+        -webkit-line-clamp: 1;
         -webkit-box-orient: vertical;
         overflow: hidden;
         text-overflow: ellipsis;

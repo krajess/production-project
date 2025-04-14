777e3933c74cd7223d1f98190a26736395135746
krajess <krajes022@gmail.com>
2025-04-14 19:00:52 +0100

    add vendor data to VendorSeeder with corrected descriptions and emails

--- database/seeders/VendorSeeder.php
+++ database/seeders/VendorSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class VendorSeeder extends Seeder
 {
@@ -12,6 +13,33 @@ class VendorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('vendors')->insert([
+            [
+                'name' => 'Johnny Electronics Ltd.',
+                'description' => 'Johnny Electronics Ltd. example description.',
+                'owner_id' => 2,
+                'email' => 'john.smith@example.com',
+                'visible' => 1,
+                'background_color' => '#ffffff',
+                'text_color' => '#000000',
+                'description_text_color' => '#000000',
+                'button_text_color' => '#ffffff',
+                'button_background_color' => '#000000',
+                'stripe_account_id' => 'acct_1RDloL2ZUdNwZB1v',
+            ],
+            [
+                'name' => 'MJ Gadgets Ltd.',
+                'description' => 'MJ Gdagets Ltd. example description.',
+                'owner_id' => 3,
+                'email' => 'micahel.johnson@example.com',
+                'visible' => 1,
+                'background_color' => '#ffffff',
+                'text_color' => '#000000',
+                'description_text_color' => '#000000',
+                'button_text_color' => '#ffffff',
+                'button_background_color' => '#000000',
+                'stripe_account_id' => 'acct_1RDm5FFQa5Lur0j1',
+            ],
+        ]);
     }
 }
